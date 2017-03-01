e43d6263cbd7f862afadc9eb808aca6f63f4af27
Radoslav Yordanov <jokuf2010@gmail.com>
2017-03-01 10:49:16 +0200

    fix

--- src/Field/Checkbox.php
+++ src/Field/Checkbox.php
@@ -19,4 +19,30 @@ class Checkbox extends Input
         parent::__construct($kwargs);
         return $this;
     }
+
+    public function __toString()
+    {
+        $options = '';
+        $tmp = "select ";
+        $tmp = array();
+        $attributes = array();
+        $checkboxes = "";
+        foreach ($this->data as $instance=>$value){
+            if($instance == 'label'){
+                continue;
+            }
+            if(is_array($value)) {
+                $checkboxes = $value;
+                continue;
+            }
+            $attributes[] = sprintf("%s=\"%s\" ", $instance, $value);
+        }
+
+        $attributes = implode(" ", $attributes);
+        foreach($checkboxes as $checkbox_value => $checkbox_name){
+            $tmp[] = sprintf("<input %s value=\"%s\">%s ", $attributes, $checkbox_value, $checkbox_name);
+        }
+        return implode("", $tmp);
+    }
 }
+
