50eb074a5d58618a72dae78dd5563ec08a291a66
Radoslav Yordanov <jokuf2010@gmail.com>
2017-03-15 10:02:36 +0200

    minor change

--- src/Field/Recaptcha.php
+++ src/Field/Recaptcha.php
@@ -21,7 +21,7 @@ class Recaptcha extends Input
     public function __toString()
     {
         $script ='<script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl='.$this->data['lang'].'"> </script>';
-        return "<div class=\"g-recaptcha\" data-sitekey=\"".$this->data['site_key']."\"></div>$secret";
+        return "<div class=\"g-recaptcha\" data-sitekey=\"".$this->data['site_key']."\"></div>".$script;
     }
 
     public function is_valid()
